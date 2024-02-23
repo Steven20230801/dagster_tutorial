4b7fc4c3586ec0ce9c561f02be3edaf7c0743cb3
DESKTOP-2825DLQ\Steven <a0922810979@gmail.com>
2024-02-23 21:51:12 +0800

    init

---
+++ etl_example.py
@@ -0,0 +1,24 @@
+import pandas as pd
+
+data = pd.DataFrame({
+    "id": range(100),
+    "score": range(100),
+    "date": pd.date_range(start='1/1/2018', periods=100, freq='D')  
+})
+def extract():
+    return data 
+
+def transform(data):
+    data['date'] = pd.to_datetime(data['date'])
+    return data
+
+def load(data) -> None:
+    data.to_csv('output.csv', index=False)
+
+def etl():
+    data = extract()
+    data = transform(data)
+    load(data)
+
+if __name__ == "__main__":
+    etl()
